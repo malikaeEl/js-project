dc9dc0f437dd9c4a6095de2ae7d166ff60d7b6d2
malikaeEl <malikaelmouden98@gmail.com>
2025-10-24 17:27:39 +0100

    solve the equation

---
+++ script.js
@@ -0,0 +1,26 @@
+// ax²+bx+c=0
+// z=b²-4ac
+
+
+
+var a =2;
+var b =2;
+var c =4;
+var z=(b*1/2)-(4*a*c);
+var z3=(-b/2*a);
+var z1=(-b+(z*1/2))/(2*a);
+var z2=(-b-(z*1/2))/(2*a);
+
+if(z=0){ 
+    alert(z1);
+    }
+else if(z>0){
+    alert()
+}
+else{
+    alert("No result")
+}
+
+
+
+
