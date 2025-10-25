7a79999cdd1eb3f9f74f5100aa618374fe3dcaa6
malikaeEl <malikaelmouden98@gmail.com>
2025-10-25 14:09:34 +0100

    modification

--- script.js
+++ script.js
@@ -3,23 +3,35 @@
 
 
 
-var a =2;
-var b =2;
-var c =4;
-var z=(b*1/2)-(4*a*c);
-var z3=(-b/2*a);
-var z1=(-b+(z*1/2))/(2*a);
-var z2=(-b-(z*1/2))/(2*a);
+// var a =0;
+// var b =0;
+// var c =0;
+// var z=(b*1/2)-(4*a*c);
+// //the equation accept 1 results
 
-if(z=0){ 
-    alert(z1);
-    }
-else if(z>0){
-    alert()
+// var z=(-b/2*a);
+
+// //the equation accept 2 results
+// var z1=(-b+(z*1/2))/(2*a);
+// var z2=(-b-(z*1/2))/(2*a);
+
+// if(z=0){ 
+//     alert(z3);
+//     }
+// else if(z>0){
+//     alert(z2);
+// }
+// else{
+//     alert("No result");
+// }
+function calculatez(a,b,c){
+  return (b*1/2)-(4*a*c)
 }
-else{
-    alert("No result")
-}
+const z=calculatez (3,7,9);
+console.log(z);
+
+function calculatez
+
 
 
 
